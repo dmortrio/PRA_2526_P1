d7689ed4784aec752b669a87ce7ed53d3d05ff43
DMORTRIO <dmortrio@alumno.upv.es@ldsic-vdi20.upvnet.upv.es>
2026-01-04 16:29:46 +0100

    Añadida implementación de la clase ListArray

---
+++ ListArray.h
@@ -0,0 +1,124 @@
+#include <ostream>
+#include "List.h"
+#include <stdexcept>
+
+template <typename T> 
+class ListArray : public List<T> {
+
+    private:
+        T* arr;
+        int max;
+        int n;
+        static const int MINSIZE = 2;
+
+        void resize(int new_size) {
+            T* new_arr = new T[new_size];
+            
+            for (int i = 0; i < n; ++i) {
+                new_arr[i] = arr[i];
+            }
+
+            delete[] arr;
+
+            arr = new_arr;
+            max = new_size;
+        }
+
+    public:
+        ListArray() {
+            arr = new T[MINSIZE];
+            max = MINSIZE;
+            n = 0;
+        }
+
+        ~ListArray() {
+            delete[] arr;
+        }
+
+        T operator[](int pos) {
+            if (pos < 0 || pos >= n) {
+                throw std::out_of_range("Posición fuera de rango");
+            }
+            return arr[pos];
+        }
+
+        friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list) {
+            out << "List => [";
+            for (int i = 0; i < list.n; ++i) {
+                out << list.arr[i];
+                if (i < list.n - 1) {
+                    out << ", ";
+                }
+            }
+            out << "]";
+            return out;
+        }
+
+        void insert(int pos, T e) override {
+            if (pos < 0 || pos > n) {
+                throw std::out_of_range("Posición de inserción inválida");
+            }
+
+            if (n == max) {
+                resize(max * 2);
+            }
+
+            // Desplaza los elementos a la derecha
+            for (int i = n; i > pos; --i) {
+                arr[i] = arr[i - 1];
+            }
+
+            // Inserta elem
+            arr[pos] = e;
+            n++;
+        }
+
+        void append(T e) override {
+            insert(n, e); 
+        }
+
+        void prepend(T e) override {
+            insert(0, e); 
+        }
+
+        T remove(int pos) override {
+            if (pos < 0 || pos >= n) {
+                throw std::out_of_range("Posición de eliminación inválida");
+            }
+
+            T elementoEliminado = arr[pos];
+
+            // Desplaza a la izq
+            for (int i = pos; i < n - 1; ++i) {
+                arr[i] = arr[i + 1];
+            }
+
+            n--;
+
+            return elementoEliminado;
+        }
+
+        T get(int pos) override {
+            if (pos < 0 || pos >= n) {
+                throw std::out_of_range("Posición inválida");
+            }
+            return arr[pos];
+        }
+
+        int search(T e) override {
+            for (int i = 0; i < n; ++i) {
+                if (arr[i] == e) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        bool empty() override {
+            return n == 0;
+        }
+
+        int size() override {
+            return n;
+        }
+};
